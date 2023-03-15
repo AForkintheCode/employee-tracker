55add013164944566c58726f99888ea5962cd09a
AForkintheCode <96763461+AForkintheCode@users.noreply.github.com>
2023-03-14 20:46:32 -0700

    editing

--- database.js
+++ database.js
@@ -3,7 +3,7 @@ var mysql = require('mysql2');
 var con = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "Bak1ng_Br3ad.",
+  password: "",
   database: "directory_db"
 });
 
